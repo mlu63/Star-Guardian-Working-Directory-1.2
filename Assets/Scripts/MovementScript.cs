81f8532b96353034a03424e60372a8c51b30c0bf
Michael Lu <michaeldelu@gmail.com>
2016-04-06 04:32:56 -0400

    scaling UI to 16:9 minimum (1024:579), astronaut animation

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour
+{
+
+    public Transform center;
+    public float degreesPerSecond = -15.0f;
+
+    private Vector3 v;
+
+    void Start()
+    {
+        v = transform.position - center.position;
+    }
+
+    void Update()
+    {
+        v = Quaternion.AngleAxis(degreesPerSecond * Time.deltaTime, Vector3.forward) * v;
+        transform.position = center.position + v;
+    }
+}
