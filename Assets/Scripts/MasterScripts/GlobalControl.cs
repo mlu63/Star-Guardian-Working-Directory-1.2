6ed8862aacc7c0c582afc7370968ef8cac0e36e8
Michael Lu <michaeldelu@gmail.com>
2016-04-05 21:20:46 -0400

    porting over changes 2.0

---
+++ Assets/Scripts/MasterScripts/GlobalControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalControl : MonoBehaviour
+{
+    public static GlobalControl Instance;
+    public bool isEasy;
+    public bool musicOn;
+
+    // default values on launch
+    void Start()
+    {
+        isEasy = true;
+        musicOn = true;
+    }
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -8,6 +8,7 @@ public class MenuScript : MonoBehaviour
     // menus
     public Canvas startMenu;
     public Canvas quitMenu;
+    public Canvas creditsMenu;
     public Canvas optionsMenu;
     public Canvas helpMenu;
 
@@ -18,6 +19,15 @@ public class MenuScript : MonoBehaviour
     public Button helpBtn;
     public Button backBtnOptions;
     public Button backBtnHelp;
+    public Button musicBtn;
+    public Button difficultyBtn;
+    public Button closeBtn;
+    public Button creditsBtn;
+
+    // music and difficulty
+    public AudioSource music;
+    private bool musicEnabled;
+    private bool isEasy;
 
     // Use this for initialization
     void Start()
@@ -26,6 +36,7 @@ public class MenuScript : MonoBehaviour
         startMenu = startMenu.GetComponent<Canvas>();
         quitMenu = quitMenu.GetComponent<Canvas>();
         optionsMenu = optionsMenu.GetComponent<Canvas>();
+        creditsMenu = creditsMenu.GetComponent<Canvas>();
         helpMenu = helpMenu.GetComponent<Canvas>();
 
         playBtn = playBtn.GetComponent<Button>();
@@ -34,12 +45,39 @@ public class MenuScript : MonoBehaviour
         helpBtn = helpBtn.GetComponent<Button>();
         backBtnOptions = backBtnOptions.GetComponent<Button>();
         backBtnHelp = backBtnHelp.GetComponent<Button>();
+        musicBtn = musicBtn.GetComponent<Button>();
+        difficultyBtn = difficultyBtn.GetComponent<Button>();
+        creditsBtn = creditsBtn.GetComponent<Button>();
+        closeBtn = closeBtn.GetComponent<Button>();
+
+        music = music.GetComponent<AudioSource>();
 
         // instantiate non-start menus to false
         startMenu.enabled = true;
         optionsMenu.enabled = false;
         quitMenu.enabled = false;
         helpMenu.enabled = false;
+        creditsMenu.enabled = false;
+
+        // set booleans for music and diff
+        musicEnabled = GlobalControl.Instance.musicOn;
+        isEasy = GlobalControl.Instance.isEasy;
+
+        // update the values and labels accordingly
+        if (!musicEnabled)
+        {
+            musicBtn.GetComponentInChildren<Text>().text = "turn music on";
+            music.enabled = false;
+            music.Stop();
+        }
+        if (!isEasy)
+        {
+            // stuff here to handle level difficulty
+            difficultyBtn.GetComponentInChildren<Text>().text = "set difficulty: easy";
+        }
+
+        //musicEnabled = true;
+        //isEasy = true;
     }
 
     // Show exit submenu and disable the buttons
@@ -62,8 +100,10 @@ public class MenuScript : MonoBehaviour
         optionsBtn.enabled = true;
     }
 
+    // Save the state into the GlobalManager and load the play scene
     public void PlayButtonPressed()
     {
+        SaveState();
         SceneManager.LoadScene("Play");
     }
 
@@ -73,6 +113,24 @@ public class MenuScript : MonoBehaviour
         // only disable the start menu since only point of access
         startMenu.enabled = false;
         optionsMenu.enabled = true;
+    }
+
+    // Display the credits screen, disable other buttons
+    public void CreditsButtonPressed()
+    {
+        creditsMenu.enabled = true;
+        backBtnOptions.enabled = false;
+        musicBtn.enabled = false;
+        difficultyBtn.enabled = false;
+    }
+
+    // Hide the credits screen, re-enable the other buttons
+    public void CloseButtonPressed()
+    {
+        creditsMenu.enabled = false;
+        backBtnOptions.enabled = true;
+        musicBtn.enabled = true;
+        difficultyBtn.enabled = true;
     }
 
     // Show the help menu by hiding others
@@ -88,6 +146,48 @@ public class MenuScript : MonoBehaviour
         startMenu.enabled = true;
         helpMenu.enabled = false;
         optionsMenu.enabled = false;
+    }
+
+    // Toggle the music
+    public void MusicButtonPressed()
+    {
+        if (musicEnabled)
+        {
+            musicEnabled = false;
+            music.enabled = false;
+            music.Stop();
+            musicBtn.GetComponentInChildren<Text>().text = "turn music on";
+            // toggle comment
+        }
+        else if (!musicEnabled)
+        {
+            musicEnabled = true;
+            music.enabled = true;
+            music.Play();
+            musicBtn.GetComponentInChildren<Text>().text = "turn music off";
+        }
+    }
+
+    // Toggle the difficulty
+    public void DifficultyButtonPressed()
+    {
+        if (isEasy)
+        {
+            isEasy = false;
+            difficultyBtn.GetComponentInChildren<Text>().text = "set difficulty: easy";
+        }
+        else
+        {
+            isEasy = true;
+            difficultyBtn.GetComponentInChildren<Text>().text = "set difficulty: hard";
+        }
+    }
+
+    // Deals with data persistence between scenes
+    public void SaveState()
+    {
+        GlobalControl.Instance.isEasy = isEasy;
+        GlobalControl.Instance.musicOn = musicEnabled;
     }
 
     // Exit the game
