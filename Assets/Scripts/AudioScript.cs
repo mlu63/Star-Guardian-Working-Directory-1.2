d75df41707a26b012129790493f14e5eeb654b2f
Michael Lu <michaeldelu@gmail.com>
2016-04-06 05:25:49 -0400

    last change for the night(morning), added sound disabling to in-game music

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AudioScript : MonoBehaviour
+{
+    public bool musicEnabled;
+    public AudioSource source;
+
+    // Use this for initialization
+    void Start () {
+        
+        source = source.GetComponent<AudioSource>();
+        musicEnabled = GlobalControl.Instance.musicOn;
+
+        if (!musicEnabled)
+        {
+            source.Stop();
+        } else if (musicEnabled)
+        {
+            source.Play();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
